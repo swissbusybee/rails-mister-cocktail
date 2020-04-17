cb369136c18f7c0cc00517b2fe894187d4050d7d
Sue Rocha <swissbusybee@gmail.com>
2020-04-17 15:33:20 +0200

    Added Controller, Views and Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "cocktails#index"
+  resources :cocktails, only: [:new, :create, :index, :show, :edit, :update] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+require 'json'
+require 'open-uri'# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database ......."
+Ingredient.destroy_all
+Cocktail.destroy_all
+
+puts "Creating ingredients and cocktails..."
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+ingredients['drinks'].each do | ingredient |
+  Ingredient.create(
+    name: ingredient['strIngredient1']
+    )
+end
+
+Cocktail.create(name: "Gin & Tonic")
+Cocktail.create(name: "Tipperary")
+Cocktail.create(name: "Penicillin")
+Cocktail.create(name: "Ruby Tuesday")
+Cocktail.create(name: "Arctic Mouthwash")
+
+puts "Finished!"
